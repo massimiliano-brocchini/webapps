441df4d2fcc257473000ee1ff11edbdde5ae54f0
Massimiliano Brocchini <massimiliano.brocchini@gmail.com>
2015-11-05 22:47:31 +0100

    fixed syntax error + added PATH variable for crontjob usage

--- massi_dns/massi_dns_client.sh
+++ massi_dns/massi_dns_client.sh
@@ -1,5 +1,6 @@
 #!/bin/zsh
 
+PATH=/usr/local/sbin:/usr/local/bin:/usr/bin
 URL=
 
 ip=$(curl http://$URL/myip.php)
@@ -9,6 +10,5 @@ if [[ "$ip" != "$last_ip" ]]; then
 	if [[ "$(curl -X POST --data-ascii "pc=$HOST" --data-ascii "otp=$(head -n 1 ./otps)" http://$URL/index.php)" -eq "ACK" ]]; then
 		sed -e 1d -i ./otps
 		echo $ip > ./last_update
-		fi
 	fi
 fi
